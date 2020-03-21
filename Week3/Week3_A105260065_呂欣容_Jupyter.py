5376c2e3f37f5a9786daebf4bfadb4c653272250
Lu-HsinJung <61856882+Lu-HsinJung@users.noreply.github.com>
2020-03-22 01:05:22 +0800

    Add files via upload

---
+++ Week3/Week3_A105260065_呂欣容_Jupyter.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# 隨堂練習：將電影(https://www.imdb.com/title/tt4154796/) 頁面中的三個資訊儲存為物件並且印出來，片長（以分鐘為單位)、評分、片名，並使用 type() 函數將這三個物件的類別印出來
+
+# In[1]:
+
+
+movie_title = "Avengers: Endgame"
+movie_length = 181
+movie_rating = 8.4
+
+print(movie_title)
+print(movie_length)
+print(movie_rating)
+
+print(type(movie_title))
+print(type(movie_length))
+print(type(movie_rating))
+
+
+# In[ ]:
+
+
+
+
